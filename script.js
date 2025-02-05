c2cd0e1aacc045d613c13fa4cdf8a99661919f91
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-02-05 14:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+
+document.getElementById("btn").addEventListener('click',()=>{
+	displayData();
+})
+
+async function displayData(){
+	const value=document.getElementById("text").value;
+	const delayVal=parseInt(document.getElementById("delay").value);
+	const delay=delayVal*1000;
+	let out=document.getElementById("output");
+	setTimeout(()=>{
+		out.textContent=`${value} - ${delayVal}`;
+	},delay)
+}
